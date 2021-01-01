a16c4e134870fe9cd489b688cc754ba2ed728253
raquellaur <raquel.navega@gmail.com>
2021-01-01 23:06:45 +0100

    quete 20

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -18,25 +18,25 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        $contributor = new User();
-        $contributor->setEmail('contributor1@monsite.com');
-        $contributor->setRoles(['ROLE_CONTRIBUTOR']);
-        $contributor->setPassword($this->passwordEncoder->encodePassword(
-            $contributor,
+        $contributor1 = new User();
+        $contributor1->setEmail('contributor1@monsite.com');
+        $contributor1->setRoles(['ROLE_CONTRIBUTOR']);
+        $contributor1->setPassword($this->passwordEncoder->encodePassword(
+            $contributor1,
             'contributor1password'
         ));
-        $manager->persist($contributor);
-        $this->addReference('contributor1', $contributor);
+        $manager->persist($contributor1);
+        $this->addReference('contributor1', $contributor1);
 
-        $contributor = new User();
-        $contributor->setEmail('contributor2@monsite.com');
-        $contributor->setRoles(['ROLE_CONTRIBUTOR']);
-        $contributor->setPassword($this->passwordEncoder->encodePassword(
-            $contributor,
+        $contributor2 = new User();
+        $contributor2->setEmail('contributor2@monsite.com');
+        $contributor2->setRoles(['ROLE_CONTRIBUTOR']);
+        $contributor2->setPassword($this->passwordEncoder->encodePassword(
+            $contributor2,
             'contributor2password'
         ));
-        $manager->persist($contributor);
-        $this->addReference('contributor2', $contributor);
+        $manager->persist($contributor2);
+        $this->addReference('contributor2', $contributor2);
 
 
         $admin = new User();
